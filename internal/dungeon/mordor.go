47491deda758feb26aa44789b12c26fb57e07458
Diogo Lourenco Andrade <127452332+soothsayerdev@users.noreply.github.com>
2025-05-26 23:05:39 -0300

    create mordor dungeon

---
+++ internal/dungeon/mordor.go
@@ -0,0 +1,25 @@
+package dungeon
+
+type Mordor struct {
+	person person // dependencia injetada via interface, desacoplada de implementacoes concretas
+	name string
+	Type string 
+}
+
+func NewMordorDung(name string, person person) *Mordor {
+	return &Mordor{
+		name: name,
+		Type: "Mordor",
+		person: person,
+	}
+}
+
+// Name retorna o nome da dungeon
+func (m *Mordor) Name() string {
+	return m.name
+}
+
+// PersonName retorna o nome do personagem que esta na dungeon
+func (m *Mordor) PersonName() string {
+	return 
+}
